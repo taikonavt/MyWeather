3d36077c07d56eb625d4c8190c70d35c679c9032
taikonavt <taikonavt@gmail.com>
2018-07-20 15:02:53 +0300

    create app, add collapsing toolbar, add nav drawer

---
+++ app/src/main/java/com/example/maxim/myweather/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.maxim.myweather;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String TAG = "myTag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        updateDrawersItem(navigationView, getFakeArray());
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void updateDrawersItem(NavigationView drawer, ArrayList<String> arrayList) {
+        int FAVOURITES_ID = 1;
+        int GROUP_ID = 1;
+        int ITEM_ID = 100;
+
+        Menu menu = drawer.getMenu();
+        MenuItem favourites = menu.getItem(FAVOURITES_ID);
+        Menu menuFavourites = favourites.getSubMenu();
+        menuFavourites.removeGroup(GROUP_ID);
+        for (int i = 0; i < arrayList.size(); i++) {
+            final MenuItem menuItem = menuFavourites.add(GROUP_ID, i, ITEM_ID + i, arrayList.get(i))
+            .setIcon(R.drawable.ic_place_black_24dp)
+            .setActionView(R.layout.action_view_delete);
+
+            View actionView = menuItem.getActionView();
+            ImageView imageView = (ImageView) actionView.findViewById(R.id.iv_delete);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // TODO: 20.07.18 Realize deleting
+                    Log.d(TAG, "clicked: " + menuItem.getItemId());
+                }
+            });
+        }
+    }
+
+    private ArrayList<String> getFakeArray(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Moscow");
+        arrayList.add("St.Petersburg");
+        arrayList.add("N.Novgorod");
+        return arrayList;
+    }
+}
