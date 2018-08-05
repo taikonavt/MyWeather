e8c6954d27528979e9b48b7a178e569ef1dc44c9
taikonavt <taikonavt@gmail.com>
2018-08-05 14:31:31 +0300

    add retrofit classes

---
+++ app/src/main/java/com/example/maxim/myweather/Network/Clouds.java
@@ -0,0 +1,22 @@
+package com.example.maxim.myweather.Network;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by maxim on 05.08.18.
+ */
+
+public class Clouds {
+    @SerializedName("all")
+    @Expose
+    private int all;
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
---
+++ app/src/main/java/com/example/maxim/myweather/Network/NetworkUtil.java
@@ -0,0 +1,9 @@
+package com.example.maxim.myweather.Network;
+
+/**
+ * Created by maxim on 05.08.18.
+ */
+
+public class NetworkUtil {
+
+}
